178bc11a89568314d299e6d9bd91049d570d420d
Kaviraj Gosaye <kamesh_kavi@hotmail.fr>
2019-07-02 12:30:55 +0400

    assignment done

--- React tutorial/react-complete-guide/src/index.js
+++ React tutorial/react-complete-guide/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import Assignment from './ReactAssign/Assignment';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<Assignment />, document.getElementById('root'));
 registerServiceWorker();
