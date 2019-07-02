e4e00457b984ccaac80f467c58f5095e7cd116c8
Kaviraj Gosaye <kaviraj.gosaye@proximitybbdo.fr>
2019-07-02 09:28:09 +0400

    Practiced Tutorial

---
+++ React tutorial/react-complete-guide/src/App.js
@@ -0,0 +1,112 @@
+import React, {Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  
+  state={
+      persons : [
+        {id:"ab",name:'Paul',age:15},
+        {id:"cd",name:'Rita',age:25},
+        {id:"ef",name:'Mary',age:85}
+      ],
+      showPersons:true
+     
+  };
+
+  /* The following is just a testing state
+     const [otherVal,setOtherVal]=useState({
+     otherVal: 'sdsdsndsbvdh'
+    })
+
+     console.log(myState,otherVal);
+  */
+
+  /* This handler is no longer used- was used to change name 
+  
+  switchBtnHandler = (changeName)=>{
+    this.setState({
+      person:[
+        {name:'Gaby',age:22},
+        {name:changeName,age:22},
+        {name:'Max',age:33},
+        {name:'King',age:28}
+      ]
+    })
+  };
+*/
+
+  //This handler will change dynamically and respectively the name when text is input
+
+  nameUpdater= (event,id) => {
+    const personIndex =this.state.persons.findIndex(p =>{
+
+      return p.id===id;
+    });
+
+    const person={
+      ...this.state.persons[personIndex]
+    }
+    person.name=event.target.value;
+    const persons=[...this.state.persons]
+    persons[personIndex]=person;
+
+    this.setState({
+      persons:persons
+    });
+  }
+
+
+  //This handler will toggle the block of content
+
+  toggleContentHandler =() =>{
+    const doesShow=this.state.showPersons;
+    this.setState({showPersons: !doesShow});
+  }
+  render(){
+
+/*
+    const style={
+      backgroundColor:'turquoise',
+      font:'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor:'pointer'
+    }
+*/
+    let persons =null;
+
+    if (this.state.showPersons){
+      persons=(
+        <div>
+          {this.state.persons.map((person) =>{
+            return <Person
+                    name={person.name}
+                    age={person.age}
+                    key={person.id}
+                    change={(event) => this.nameUpdater(event,person.id)}
+                   
+                    />
+          })}  
+        </div>
+           
+      );
+    }
+    return (
+      <div className="App">
+        <h1>Hi just creating the react app</h1>
+        <p>This is working just fine !!</p>
+        <button id="clicker"
+          onClick={this.toggleContentHandler}>
+          Hide Content !
+        </button>
+        {persons}
+       
+      </div>
+    );
+    
+  }
+};
+
+
+export default App;
