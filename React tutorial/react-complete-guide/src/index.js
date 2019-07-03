c6996a1c01341f5dee76167d6390ecf17cbcae5f
Kaviraj Gosaye <kamesh_kavi@hotmail.fr>
2019-07-03 15:57:36 +0400

    Re-structured folders

--- React tutorial/react-complete-guide/src/index.js
+++ React tutorial/react-complete-guide/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
 import Assignment from './ReactAssign/Assignment';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App appTitle="HR Dashboard"/>, document.getElementById('root'));
 //ReactDOM.render(<Assignment />, document.getElementById('root'));
 registerServiceWorker();
